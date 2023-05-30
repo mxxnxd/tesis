ad52da6e4b97c90af4ba7b0bb6dcdc49a04dd944
Steven Castro <steven.castro.fy@gmail.com>
2023-05-30 16:50:38 +0800

    State tracking #2 (small fix)

--- fulfillment/language.js
+++ fulfillment/language.js
@@ -1,7 +1,6 @@
 const util = require('./utility.js');
 const db = require('../firebase/database.js');
-const { getUserState, setUserState } = require('../firebase/state.js');
-// const stateManager = require('../model/state-manager.js'); 
+const { delUserState, getUserState, setUserState } = require('../firebase/state.js');
 
 /*
 	Webhook Functions: Language Selection Intents 
