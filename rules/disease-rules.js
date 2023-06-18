76eb854d604a80672c5030d39f6c2aecf45d803e
Steven Castro <steven.castro.fy@gmail.com>
2023-06-18 20:59:32 +0800

    Moved determineInitialDisease inside getAction Removed unwanted old code comments

--- rules/disease-rules.js
+++ rules/disease-rules.js
@@ -1,101 +1,16 @@
-
-const { Action, Disease, Symptom } = require('./class.js');
-
 const fs = require('fs');
 
-// get data from json file
-const jsonData = fs.readFileSync('./fulfillment/disease-symptom.json', 'utf8');
+// Parse Symptom-Disease 
+const jsonData = fs.readFileSync('../fulfillment/disease-symptom.json', 'utf8');
 const disease_symptom = JSON.parse(jsonData);
-
-
 
 /* ===== ===== ===== ===== ===== ===== ===== */
 
 const PRIORITY = 100;
 
-// const COPD = 'COPD';
-const ASTHMA = 'ASTHMA';
-const PNEUMONIA = 'PNEUMONIA';
-const LUNG_CANCER = 'LUNG_CANCER';
-const TUBERCULOSIS = 'TUBERCULOSIS';
-
-const HEARTFAILURE = 'HEART_FAILURE';
-const HYPERTENSION = 'HYPERTENSION';
-const CAD = 'CAD';
-const ARRHYTHMIA = 'ARRHYTHMIA';
-const CARDIOMYOPATHY = 'CARDIOMYOPATHY'
-const VALVEDISEASE = 'VALVE_DISEASE';
-const MYOCARDIALIN_FARCTION = 'MYOCARDIAL_INFARCTION';
-const ANEURYSM = 'ANEURYSM';
-
 /* ===== ===== ===== ===== ===== ===== ===== */
 
-const diseases = [];
-
-const COPD = new Disease('COPD', ['chest_pain', 'dyspnea', 'wheeze', 'phlegm']);
-diseases.push(COPD);
-
-/* ===== ===== ===== ===== ===== ===== ===== */
-
-// determine which disease to focus on after initial input
-// const determineInitialDisease = (facts) => {
-// 	const matches = [];
-// 	const user_symptoms = facts.positive_symptoms;
-// 	for (const {disease, symptom} of disease_symptom) {
-// 		// uses every() so it wont recognize if user input is from different diseases 
-// 		if (user_symptoms.every(temp => symptom.includes(temp.toLowerCase()))) { // we could change the json file to all caps, otherwise keep toLowerCase()
-// 			matches.push(disease);
-// 		}
-// 	}
-// 	//console.log('matches: ' + matches)
-// 	return {matches, user_symptoms};
-// }
-
-// const prioritizeDiseasesRules = {
-// 	id: 'priority',
-// 	priority: PRIORITY + 50,
-// 	condition: (R, facts) => {
-// 		R.when(facts.user.start);	// when the initial input starts
-// 	},
-// 	consequence: (R, facts) => {
-// 		const initial = determineInitialDisease(facts.user);
-// 		console.log(initial)
-// 		for (i = 0; i < initial.matches.length; i++) {
-// 			R.prioritize(PRIORITY + 49 - i, {id: initial.matches[i]});
-// 		}
-// 		facts.user.start = false;
-// 		R.stop();
-// 		// R.next();
-// 	}
-// }
-
-const recall_rule = {
-	id: 'recall',
-	priority: 200,
-	condition: (R, facts) => {
-		var previous_symptoms = facts.user.previous_symptoms;
-		R.when(previous_symptoms.length != 0);
-	},
-	consequence: (R, facts) => { 
-		var previous_symptoms = facts.user.previous_symptoms;
-		var investigated_symptom = previous_symptoms.shift();
-
-		facts.previous_symptoms = previous_symptoms;
-		facts.agent.next_action = `RECALL-${investigated_symptom.toUpperCase()}`;
-
-		R.stop();
-	}
-}
-
-const setRecurringUserRules = () => {
-	
-}
-
-const possibleDiseasesRules = [];
-const diagnoseDiseasesRules = [];
-// update facts next action
-const setDiseasesRules = (R) => {
-
+const registerDiseaseRules = (R) => {
 	for (let i = 0; i < disease_symptom.length; i++) {
 		let id = disease_symptom[i].disease;
 		let symptoms = disease_symptom[i].symptom;
@@ -147,44 +62,23 @@ const setDiseasesRules = (R) => {
 	}
 }
 
-// Update facts w/ possible diseases based on current symptoms.
-const determinePossibleDiseases = {
-	priority: PRIORITY + 10,
+const recallSymptoms = {
+	id: 'recall',
+	priority: 200,
 	condition: (R, facts) => {
-		R.when(true);
+		var previous_symptoms = facts.user.previous_symptoms;
+		R.when(previous_symptoms.length != 0);
 	},
-	consequence: (R, facts) => {
-		diseases.forEach(disease => {
-			for (i = 0; i < disease.symptoms.length; i++) {
-				const current_symptom = facts.user.symptoms[disease.symptoms[i]];
-				if (current_symptom || current_symptom === undefined) {
-					facts.user.possible_diseases[disease.name] = true;
-				} else {
-					delete facts.user.possible_diseases[disease.name];
-					break;
-				}
-			}
-		});
-		R.next();
-	}
-};
+	consequence: (R, facts) => { 
+		var previous_symptoms = facts.user.previous_symptoms;
+		var investigated_symptom = previous_symptoms.shift();
 
+		facts.previous_symptoms = previous_symptoms;
+		facts.agent.next_action = `RECALL-${investigated_symptom.toUpperCase()}`;
 
-// Chronic Obstructive Pulmonary Disease
-const diagnoseCOPD = {
-	id: "diagnoseCOPD",
-	priority: PRIORITY - 50,
-	condition: (R, facts) => {
-		// Check possbile disease
-		// if (facts.user.possible_diseases['COPD']) {
-		R.when(checkAllSymptoms(facts, 'copd'));
-		// }
-		R.when(false);
-	},
-	consequence: (R, facts) => {
 		R.stop();
 	}
-};
+}
 
 const hasAction = {
 	id: 'hasAction',
@@ -212,60 +106,12 @@ const diagnoseNone = {
 
 const applyRules = (R) => {
 	//R.register(prioritizeDiseasesRules);
-	R.register(recall_rule);
-	setDiseasesRules(R);
-
-	//R.register(diagnoseCOPD);
+	R.register(recallSymptoms);
 	R.register(hasAction);
 	R.register(diagnoseNone);
+	registerDiseaseRules(R);
 };
 
 module.exports = {
 	applyRules
 };
-
-function checkAllSymptoms(facts, disease) {
-	if (!checkSymptoms(facts, disease)) {
-		return false;
-	}
-	console.log('Key Symptoms: PASSED');
-
-	return true;
-}
-
-function checkSymptoms(facts, disease) {
-	const current_symptoms = facts.user.symptoms;
-	const disease_symptoms = facts.agent.questions[disease];
-
-	// Check symptoms of this category
-	const category_symptoms = disease_symptoms;
-	var false_key_symptoms = 0;
-	var threshhold = 4; 								// Atleast n symptoms, fetched from KB
-	var length = category_symptoms.length;
-						
-	for (i = 0; i <  length; i++) {
-		let symptom = category_symptoms[i];
-		
-		if (current_symptoms[symptom] === undefined) { 	// User has not determined this symptom. 					
-			facts.agent.next_action = new Action('ASK', symptom.toUpperCase(), disease_symptoms[category][symptom].type, 'INIT');
-			return false;
-
-		} else if (current_symptoms[symptom].value) {	// User has determined this symptom to be True.					
-			let current_type = current_symptoms[symptom].type;
-			let disease_type = disease_symptoms[category][symptom].type;
-
-			if (current_type !== disease_type && disease_type === 'RATE') {
-				facts.agent.next_action = new Action('ASK', symptom.toUpperCase(), disease_type, 'REDO');
-				return false;
-			} 
-		} else { 										// User has determined this symptom to be False.
-			false_key_symptoms++;
-
-			// Threshold to Pass?						// Find a Threshold
-			if (length - threshhold <= false_key_symptoms) {
-				return false;
-			}
-		}															
-	}
-	return true;
-}
--- rules/rules-engine.js
+++ rules/rules-engine.js
@@ -1,8 +1,8 @@
 
 // Libraries
 const { RuleEngine } = require('node-rules');
-
-const { Symptom, Action, Disease } = require('./class.js');
+const fs = require('fs');
+// const { getUserState, setUserState } = require('../firebase/state.js');
 
 // Rules definition
 const diseasesRuleset = require('./disease-rules.js');
@@ -14,12 +14,24 @@ R.ignoreFactChanges = true;
 // Apply rules
 diseasesRuleset.applyRules(R);
 
-
 /*
 	Updates facts with the next agent action based on current facts.
 */
 const getAction = (inputFacts) => {
 	const facts = inputFacts;
+
+	// First Run (Reorders Rules)
+	if (facts.user.start) {
+		const PRIORITY = 100;
+		const initial = determineInitialDisease(facts.user);
+		
+		for (i = 0; i < initial.matches.length; i++) {
+			R.prioritize(PRIORITY + 49 - i, {id: initial.matches[i]});
+		}
+		console.log('> Rules Reordered...');
+		facts.user.start = false;
+	}
+
 	return new Promise((resolve, reject) => {
 		R.execute(facts, (data) => {
 			resolve(data);
@@ -31,18 +43,13 @@ module.exports = {
 	getAction
 };
 
+// Determine which disease to focus on after initial input
+const determineInitialDisease = (facts) => {
+	const jsonData = fs.readFileSync('../fulfillment/disease-symptom.json', 'utf8');
+	const disease_symptom = JSON.parse(jsonData);
 
-const fs = require('fs');
-const { getUserState, setUserState } = require('../firebase/state.js');
-// get data from json file
-const jsonData = fs.readFileSync('./fulfillment/disease-symptom.json', 'utf8');
-const disease_symptom = JSON.parse(jsonData);
-
-
-// determine which disease to focus on after initial input
-const determineInitialDisease = (params) => {
 	const matches = [];
-	const user_symptoms = params.positive_symptoms;
+	const user_symptoms = facts.positive_symptoms;
 	for (const {disease, symptom} of disease_symptom) {
 		// uses every() so it wont recognize if user input is from different diseases
 		if (user_symptoms.every(temp => symptom.includes(temp.toLowerCase()))) { // we could change the json file to all caps, otherwise keep toLowerCase()
@@ -53,48 +60,24 @@ const determineInitialDisease = (params) => {
 	return {matches, user_symptoms};
 }
 
-var facts = {
-	user: {
-		positive_symptoms: [],
-		negative_symptoms: [],
-		previous_symptoms: [],
-		start: true,
-	},
-	agent: {}
-};
-
-const main = async (questions) => {
+// Testing
+const main = async () => {
 	var facts = {
 		user: {
-			positive_symptoms: [],
+			positive_symptoms: ['blurry'],
 			negative_symptoms: [],
-			previous_symptoms: ['blurry'],
-			start: false,
+			previous_symptoms: [],
+			start: true,
 		},
 		agent: {}
 	};
 
+	if (facts.user.previous_symptoms.length === 0) {
+		facts.user.start = true;
+	}
+
 	var prompter = require("prompt-sync")({ sigint: '' });
-
-
-	// const PRIORITY = 100;
-	// const initial = determineInitialDisease(facts.user);
-	// for (i = 0; i < initial.matches.length; i++) {
-	// 	R.prioritize(PRIORITY + 49 - i, {id: initial.matches[i]});
-	// }
-
-
 	while (true) {
-		if (facts.user.previous_symptoms.length === 0) {
-			facts.user.start = true;
-		}
-		if (facts.user.start) {
-			const PRIORITY = 100;
-			const initial = determineInitialDisease(facts.user);
-			for (i = 0; i < initial.matches.length; i++) {
-				R.prioritize(PRIORITY + 49 - i, {id: initial.matches[i]});
-			}
-		}
 		var output = await getAction(facts);
 		console.log(output.agent.next_action);
 
@@ -106,7 +89,6 @@ const main = async (questions) => {
 		}
 
 		var input = prompter(`>:`).toLowerCase();
-		
 		if (input === 'y') {
 			output.user.positive_symptoms.push(output.agent.next_action.split('-')[1].toLowerCase());
 		} else if (input === 'n') {
@@ -117,118 +99,8 @@ const main = async (questions) => {
 		}
 
 		facts = output;
-		// var initial2 = determineInitialDisease(facts.user);
-		// for (i = 0; i < initial2.matches.length; i++) {
-		// 	R.prioritize(PRIORITY + 49 - i, {id: initial2.matches[i]});
-		// }
 		console.log(facts.user);
 	}
-
-
-
-
-
-	// console.log('---------------------------------------')
-	// console.log(R.activeRules)
-	// console.log('---------------------------------------')
-
-
-	// facts.agent['questions'] = questions;
-
-	// const prompt = require("prompt-sync")({ sigint: '' });
-
-
-	// console.log(await getAction(facts));
-
-	// while (true) {
-	// 	delete facts.agent.next_action;
-	// 	// console.log(checkAllSymptoms(facts, 'asthma'));
-	// 	checkAllSymptoms(facts, 'asthma')
-	// 	let action = facts.agent.next_action;
-
-	// 	if (action === undefined) {
-	// 		break;
-	// 	}
-	// 	console.log(action.string());
-		
-	// 	let input = prompt(`>:`).toLowerCase();
-
-	// 	if (input === 'x') {
-	// 		break;
-	// 	} else {
-	// 		facts.user.symptoms[action.topic.toLowerCase()] = action.subtopic;
-	// 	}
-	// 	 console.log(facts.user.symptoms);	
-	// }
-	// console.log(facts);
 };
 
-const questions = disease_symptom;
-
-// console.log(facts)
-// setUserState('here', facts)
-main(questions);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function checkAllSymptoms(facts, disease) {
-	if (!checkSymptoms(facts, disease)) {
-		return false;
-	}
-	console.log('Key Symptoms: PASSED');
-
-	return true;
-}
-
-function checkSymptoms(facts, disease) {
-	const current_symptoms = facts.user.symptoms;
-	const disease_symptoms = facts.agent.questions.find(obj => obj.disease === disease);
-	// console.log('here')
-	// console.log(disease_symptoms.symptom.length)
-	// Check symptoms of this category
-	const category_symptoms = disease_symptoms.symptom;
-	var false_key_symptoms = 0;
-	var threshhold = 4; 								// Atleast n symptoms, fetched from KB
-	var length = category_symptoms.length;
-						
-	for (i = 0; i <  length; i++) {
-		let symptom = category_symptoms[i];
-		let next_symptom =  category_symptoms[i + 1];
-		if (current_symptoms[symptom] === undefined) { 	// User has not determined this symptom. 
-			console.log('ask ' + symptom.toUpperCase());					
-			facts.agent.next_action = new Action('ASK', symptom.toUpperCase());
-			return false;
-		} else if (current_symptoms[symptom].value) {	// User has determined this symptom to be True.
-			facts.agent.next_action = new Action('ASK', next_symptom.toUpperCase());			
-			return false;	
-		} else { 										// User has determined this symptom to be False.
-			false_key_symptoms++;
-			console.log('false key symptom = ' + false_key_symptoms)
-
-			// Threshold to Pass?						// Find a Threshold
-			if (length - threshhold <= false_key_symptoms) {
-				console.log('pass? ' +  (length - threshhold) + ' <= ' + false_key_symptoms);
-				return false;
-			}
-		}															
-	}
-	return true;
-}
+main();
